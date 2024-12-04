ea472496ab690478b1b9a5c9a29f3479e12adb13
erdal <erdalcildir03@gmail.com>
2024-12-04 19:52:43 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:false,
+    nav:true,
+    dots:false,
+     responsive:{
+         0:{
+    
+            items:1
+         },
+        600:{
+    
+            items:3
+         },
+         1000:{
+            items:6
+         }
+     }
+})
+   
